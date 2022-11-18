bd2e2db30027c47673f00e90792942c5c500df34
wzeeshan <wajahatzeeshan@gmail.com>
2022-11-18 19:19:15 +0000

    adding all

---
+++ hello_world.py
@@ -0,0 +1,31 @@
+def hello_world():
+    return "Hello World!"
+
+
+print(hello_world())
+
+
+def myfunc():
+    global x
+    x = "fantastic"
+    return x
+
+
+print(myfunc())
+
+x = 5
+print(type(x))
+
+var = 17 + 5
+print(var)
+
+""""Quote me"""
+a = "Hello"
+b = a
+a = a + " World"
+print(a)
+print(b)
+
+# a={str}'Hello World'
+# b={str}'Hello'
+# test = {str}'Hello'
